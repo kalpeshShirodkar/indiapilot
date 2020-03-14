bd5fb699e326a4c06e4d79e128fc7fad8ec40960
kalpesh <shirodkarkalpesh34@gmail.com>
2020-03-14 13:26:34 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './templet/header/header/header.component';
+import { FooterComponent } from './templet/footer/footer/footer.component';
+import { ProductComponent} from './products/product/product.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import {AdminRouteModule} from './products/admin-route/admin-route.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+ 
+    DashboardComponent,
+ 
+    ProductComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AdminRouteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
